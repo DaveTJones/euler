8b76fae4ab6da57fb6a824ab74d51cd35a20b272
Dave Jones <davetjones1992@gmail.com>
2023-10-19 09:09:55 +0100

    Cleans up Fibbonaci code and solves factorisation problem

--- problem_2/src/main.rs
+++ problem_2/src/main.rs
@@ -1,15 +1,15 @@
+// Define fibbonaci struct with upper bound to determine highest returnable value
 struct Fib {
     current: u64,
     next: u64,
-    bound: u64,
 }
 
+// only interested in even values, so initialise with first two even Fibbonaci numbers
 impl Fib {
     fn new() -> Fib {
         Fib {
             current: 2,
             next: 8,
-            bound: 4_000_000,
         }
     }
 }
@@ -18,10 +18,9 @@ impl Iterator for Fib {
     type Item = u64;
 
     fn next(&mut self) -> Option<Self::Item> {
-        if self.bound < self.current {
-            return None;
-        }
         let current = self.current;
+
+        // modified Fibbonaci formula to return only sequential even terms
         self.current = self.next;
         self.next = 4 * self.next + current;
         Some(current)
@@ -29,6 +28,7 @@ impl Iterator for Fib {
 }
 
 fn main() {
-    let test: u64 = Fib::new().take(103).sum();
+    let bound = 4_000_000;
+    let test: u64 = Fib::new().take_while(|x| x < &bound).sum();
     println!("{test:?}");
 }
